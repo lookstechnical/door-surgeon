be3424980083801c0452dfb0291152712a0487ba
Richard Ellis <info@lookstechncial.co.uk>
2025-01-22 23:16:40 +0000

    fix

--- components/cms/stacked-content-block.tsx
+++ components/cms/stacked-content-block.tsx
@@ -14,19 +14,19 @@ export const StackedContentBlock = ({
           variant="FadeInBottom"
           className="flex items-center justify-center flex-col md:px-40 text-center"
         >
-          <p className="pt-4">{highlight}</p>
-          <h1 className="text-7xl ">{heading}</h1>
-          <p className="pt-4">{desc}</p>
+          <p className="pt-4 text-primary text-lg">{highlight}</p>
+          <h1 className="text-6xl">{heading}</h1>
+          <p className="pt-4 text-lg">{desc}</p>
         </Animate>
         <div className="w-full pt-10 ">
           <Animate variant="FadeInBottom" delay={0.25} className="">
-            <div className="relative aspect-1/1 md:aspect-[3/1] rounded-xl overflow-hidden">
+            <div className="relative aspect-[1/1] md:aspect-[3/1] rounded-xl overflow-hidden">
               <Image
                 alt="image"
-                width={100}
+                width={300}
                 height={100}
-                src={image}
-                className="w-full h-full object-none"
+                src={`/${image}`}
+                className="w-full h-full object-cover"
               />
             </div>
           </Animate>
