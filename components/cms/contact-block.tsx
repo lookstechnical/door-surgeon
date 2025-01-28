d4ccf95e2607447b3d278769ad48b8610f55d3cd
Richard Ellis <info@lookstechncial.co.uk>
2025-01-28 20:02:09 +0000

    fix

--- components/cms/contact-block.tsx
+++ components/cms/contact-block.tsx
@@ -5,33 +5,35 @@ import { Whatsapp } from "../icons/whatsapp";
 
 export const ContactBlock = ({ heading, desc, highlight, image }: any) => {
   return (
-    <section className="container max-w-[1200px] mx-auto py-10 flex flex-col md:flex-row overflow-hidden px-4">
-      <div className="md:w-1/2 pr-20 animate-fadeInLeft duration-500">
-        {highlight && <p className="text-primary pb-2">{highlight}</p>}
-        <h2 className="text-5xl">{heading}</h2>
-        <p className="pt-6">{desc}</p>
-        <div className="flex space-x-4">
-          <a
-            href="https://www.facebook.com/pages/The-door-surgeon/364962176914907"
-            className="text-primary-200 hover:text-primary-400"
-          >
-            <Facebook className="h-6 w-6" />
-          </a>
-          <a
-            href="https://api.whatsapp.com/send/?phone=07912294111"
-            className="text-primary-200 hover:text-primary-400"
-          >
-            <Whatsapp className="h-6 w-6" />
-          </a>
-        </div>
-      </div>
-      <div className="md:pl-10 md:w-1/2 py-4">
-        <Animate variant="FadeInBottom">
-          <div className="rounded-xl overflow-hidden">
-            <img src={image} />
+    <div className="w-full bg-primary/5">
+      <section className="container max-w-[1200px] mx-auto py-10 flex flex-col md:flex-row overflow-hidden px-4 ">
+        <div className="md:w-1/2 pr-20 animate-fadeInLeft duration-500">
+          {highlight && <p className="text-primary pb-2">{highlight}</p>}
+          <h2 className="text-5xl">{heading}</h2>
+          <p className="pt-6">{desc}</p>
+          <div className="flex space-x-4 mt-4">
+            <a
+              href="https://www.facebook.com/pages/The-door-surgeon/364962176914907"
+              className="text-primary hover:text-primary-400"
+            >
+              <Facebook className="h-12 w-12" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=07912294111"
+              className="text-primary hover:text-primary-400"
+            >
+              <Whatsapp className="h-12 w-12" />
+            </a>
           </div>
-        </Animate>
-      </div>
-    </section>
+        </div>
+        <div className="md:pl-10 md:w-1/2 py-4">
+          <Animate variant="FadeInBottom">
+            <div className="rounded-xl overflow-hidden">
+              <img src={image} />
+            </div>
+          </Animate>
+        </div>
+      </section>
+    </div>
   );
 };
