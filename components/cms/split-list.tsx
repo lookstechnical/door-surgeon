924c7586a71b31c88a2e64b942f059fb2bbbb2b5
Richard Ellis <info@lookstechncial.co.uk>
2025-02-10 19:39:55 +0000

    update design elements

--- components/cms/split-list.tsx
+++ components/cms/split-list.tsx
@@ -11,32 +11,42 @@ export const SplitList = ({
   color,
 }: any) => {
   return (
-    <section
-      className={`${
-        layout === "left" ? "md:flex-row-reverse" : "md:flex-row"
-      } ${color} container max-w-[1200px] mx-auto py-10 flex flex-col overflow-hidden px-4`}
-    >
-      <div className="md:w-1/2 pr-20 animate-fadeInLeft duration-500">
-        {highlight && <p className="text-primary pb-2">{highlight}</p>}
-        <h2 className="text-5xl">{heading}</h2>
-        <p className="pt-6">{desc}</p>
-      </div>
-      <div className="md:pl-10 md:w-1/2 py-4">
-        <ul className="flex w-full flex-col md:flex-row flex-wrap gap-10 overflow-hidden">
-          {items?.map((item: any) => (
-            <Animate
-              as="li"
-              variant="FadeInBottom"
-              className="max-w-full md:max-w-1/2"
-            >
-              <div className="rounded-xl w-10 h-10 bg-primary/50 flex items-center justify-center text-primary">
-                <Check />
-              </div>
-              <h3 className="pt-4 pb-2">{item.heading}</h3>
-              <p>{item.description}</p>
-            </Animate>
-          ))}
-        </ul>
+    <section className={`${color} py-4`}>
+      <div
+        className={`${
+          layout === "left" ? "md:flex-row-reverse" : "md:flex-row"
+        }  container max-w-[1200px] mx-auto py-10 flex flex-col overflow-hidden px-4`}
+      >
+        <div
+          className={`${
+            layout === "left" ? "text-right" : "text-left"
+          } md:w-1/2 pr-20 animate-fadeInLeft duration-500`}
+        >
+          {highlight && <p className="text-primary pb-2">{highlight}</p>}
+          <h2 className="text-5xl">{heading}</h2>
+          <p className="pt-6">{desc}</p>
+        </div>
+        <div
+          className={`${
+            layout === "left" ? "md:pr-10" : "md:pl-10"
+          } md:w-1/2 py-4`}
+        >
+          <ul className="flex w-full flex-col md:flex-row flex-wrap gap-10 overflow-hidden">
+            {items?.map((item: any) => (
+              <Animate
+                as="li"
+                variant="FadeInBottom"
+                className="max-w-full md:max-w-1/2"
+              >
+                <div className="rounded-xl w-10 h-10 bg-primary/50 flex items-center justify-center text-primary">
+                  <Check />
+                </div>
+                <h3 className="pt-4 pb-2">{item.heading}</h3>
+                <p>{item.description}</p>
+              </Animate>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
--- content/pages/door-and-window-installations.md
+++ content/pages/door-and-window-installations.md
@@ -15,7 +15,8 @@ content:
   - type: hero
     highlight: "Beautiful, Energy-Efficient "
     heading: Windows and Doors Professionally Installed
-    desc: "We provide expert composite door and uPVC window installation services in
+    desc:
+      "We provide expert composite door and uPVC window installation services in
       West Yorkshire, enhancing homes with superior security, energy efficiency,
       and modern aesthetics. Our high-quality composite doors offer durability
       and style, while our uPVC windows improve insulation and require minimal
@@ -28,7 +29,8 @@ content:
     layout: stacked
   - type: checklist
     heading: Composite Door Installation
-    desc: Enhance your home's appearance and security with our expert composite door
+    desc:
+      Enhance your home's appearance and security with our expert composite door
       installation services in West Yorkshire. Our locally trusted team
       specialises in fitting high-quality composite doors that offer superior
       durability, style, and security for both residential and commercial
@@ -53,9 +55,10 @@ content:
       performance, helping you save on energy bills while maintaining a
       comfortable indoor environment.
     layout: left
-    color": bg-primary/10
+    color: bg-primary/10
   - type: alert
-    desc: Choosing our composite door and uPVC window installation services means
+    desc:
+      Choosing our composite door and uPVC window installation services means
       investing in quality, efficiency, and exceptional customer service. With
       years of experience serving the local community, our dedicated team
       provides tailored solutions that enhance both the functionality and
