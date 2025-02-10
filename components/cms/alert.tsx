ed5e38b71aead4f601bca7ebc63a1b2ea19f2d77
Richard Ellis <info@lookstechncial.co.uk>
2025-02-10 19:32:45 +0000

    add alert

--- components/cms/alert.tsx
+++ components/cms/alert.tsx
@@ -2,7 +2,7 @@ import { PhoneCall } from "lucide-react";
 import Link from "next/link";
 import { Animate } from "../animate/animation";
 
-export const Alert = ({ heading, highlight, description }: any) => {
+export const Alert = ({ heading, highlight, desc }: any) => {
   return (
     <section className="container mx-auto max-w-[1200px] text-white py-10 px-4">
       <Animate
@@ -11,7 +11,7 @@ export const Alert = ({ heading, highlight, description }: any) => {
       >
         {highlight && <p>{highlight}</p>}
         <h3 className="text-6xl text-white text-center">{heading} </h3>
-        <p className="">{description}</p>
+        <p className="py-4 max-w-[70%]">{desc}</p>
         <Link
           href="tel:07912 294 111"
           className="hidden md:flex bg-primary border-2 border-white mt-4 hover:bg-primary text-white mx-4 block button flex flex-row justify-center items-center py-3 px-4 rounded-xl"
