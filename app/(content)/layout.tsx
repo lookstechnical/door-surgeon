459906b568b0250065a704a3bcca53ac8134bc0a
Richard Ellis <info@lookstechncial.co.uk>
2025-02-11 14:42:48 +0000

    fix admin

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -19,28 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </head>
       <body className={inter.className}>
         <Navigation />
         {children}
         <Footer />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            if (window.netlifyIdentity) {
-              window.netlifyIdentity.on("init", user => {
-                if (!user) {
-                  window.netlifyIdentity.on("login", () => {
-                    document.location.href = "/admin/";
-                  });
-                }
-              });
-            }
-          `,
-          }}
-        />
       </body>
     </html>
   );
